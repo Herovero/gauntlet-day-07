5b982e040d1e940e0e57d1bbc27eeaca21223877
Herovero <herovero0117@gmail.com>
2026-08-15 12:34:12 +0800

    green: adding an item raises the count

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -1,7 +1,10 @@
 #include "Inventory.hpp"
 
-void Inventory::add(const std::string& item) {
+/*void Inventory::add(const std::string& item) {
     (void)item;
+}*/
+void Inventory::add(const std::string& item) { 
+    m_items.push_back(item); 
 }
 
 void Inventory::remove(const std::string& item) {
@@ -13,6 +16,9 @@ bool Inventory::has(const std::string& item) const {
     return false;
 }
 
-int Inventory::count() const {
+/*int Inventory::count() const {
     return 0;
+}*/
+int Inventory::count() const { 
+    return m_items.size(); 
 }
