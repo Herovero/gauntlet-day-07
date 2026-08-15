2590ffce4a2c753cf11ed879913cfc2cbca7ad64
Herovero <herovero0117@gmail.com>
2026-08-15 10:19:36 +0800

    Files created and ready for test

---
+++ ex00/Inventory.cpp
@@ -0,0 +1,18 @@
+#include "Inventory.hpp"
+
+void Inventory::add(const std::string& item) {
+    (void)item;
+}
+
+void Inventory::remove(const std::string& item) {
+    (void)item;
+}
+
+bool Inventory::has(const std::string& item) const {
+    (void)item;
+    return false;
+}
+
+int Inventory::count() const {
+    return 0;
+}
